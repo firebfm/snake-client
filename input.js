a83f6c38cfb8f71803573e26c040b66770ee2705
Simon Huang <firebfm@gmail.com>
2020-12-02 20:18:40 +0000

    conn object in setupInput and wasd logs

--- input.js
+++ input.js
@@ -3,15 +3,28 @@
  * Specifically, so that we can handle user input via stdin
  */
 
- // handleUserInput callback
+// Stores the active TCP connection object.
+let connection;
+
+ // callback
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log('Exit! Bye!');
     process.exit();
   }
+  if (key === 'w') {
+    console.log('Move: up');
+  } else if (key === 'a') {
+    console.log('Move: left');
+  } else if (key === 's') {
+    console.log('Move: down');
+  } else if (key === 'd') {
+    console.log('Move: right');
+  }
 }
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
