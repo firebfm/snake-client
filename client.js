6c73cdbdae6572a0cd856e4797fc1a944ff84466
Simon Huang <firebfm@gmail.com>
2020-12-02 18:57:59 +0000

    change name initials

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ const connect = function() {
 
   conn.on('connect', (client) => {
     console.log("From client: Successfully connected to game server");
-    conn.write("Name: SH");
+    conn.write("Name: SYH");
     // setInterval(() => {
     //   conn.write("Move: up")
     // }, 100);
